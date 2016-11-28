45d6733ef8997e6d687ccbe4de955c969a771fd5
Patrick W. Montgomery <pmontgo33@gmail.com>
2016-11-28 18:12:13 +0000

    In progress

--- planner/plannerutils.py
+++ planner/plannerutils.py
@@ -3,12 +3,35 @@
 # created: 11-17-2016
 
 # import statements
+from django.db.models import Min, F
 import math
 
 from planner.models import WeatherStation
 from planner.lawn import lawnutils
 
+
 def get_closest_station_data(zip_code):
+    
+
+    # get the users zip code, and look up the latitude and longitude
+    my_lat, my_long = lawnutils.get_lat_long(zip_code)
+    
+    """
+    Below finds the closest station to the zip code by finding the distance
+    between two points.
+    """
+    
+    delta_lat = my_lat - F('latitude')
+    delta_long = my_long - F('longitude')
+    distance_equation = math.sqrt(delta_lat**2+delta_long**2)
+    
+    query = WeatherStation.objects.all().aggregate(Min(distance_equation))
+    print(query)
+    exit()
+    
+#    return closest_station, closest_station.temp_data
+
+def OLD_get_closest_station_data(zip_code):
     
     stations = WeatherStation.objects.all().iterator()
     
