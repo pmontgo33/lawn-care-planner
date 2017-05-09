f26e311ae771352cecda7da2b0cc93dccbd91bc7
Patrick W. Montgomery <pmontgo33@gmail.com>
2017-05-08 22:43:14 -0400

    Created first test! Need to add real tests next
    issue #35

--- planner/tests.py
+++ planner/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+"""
+This file contains all of the tests for the planner django app
+"""
 
-# Create your tests here.
+# Import Statements
+from django.test import TestCase
+from django.contrib.auth.models import User
+from planner.models import Lawn, GrassType
+
+
+class PlannerViewsTestCase(TestCase):
+
+    def test_index(self):
+        """
+        test if homepage (index) loads.
+        :return: 
+        """
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    #TODO: Add real tests
