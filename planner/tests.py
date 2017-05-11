8becfb693ce1eabbe766e06a32bc0c37215ae575
Patrick W. Montgomery <pmontgo33@gmail.com>
2017-05-10 21:20:51 -0400

    Adds detail view test

--- planner/tests.py
+++ planner/tests.py
@@ -9,6 +9,7 @@ from planner.models import Lawn, GrassType
 
 
 class PlannerViewsTestCase(TestCase):
+    fixtures = ['planner_views_testdata.json', 'auth_views_testdata.json']
 
     def test_index(self):
         """
@@ -18,4 +19,16 @@ class PlannerViewsTestCase(TestCase):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
 
-    #TODO: Add real tests
+    def test_detail(self):
+        """
+        test if lawn detail page loads.
+        :return: 
+        """
+        resp = self.client.get('/planner/lawn/1/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.context['lawn'].pk, 1)
+        self.assertEqual(resp.context['lawn'].name, "ORELAND PA Lawn")
+
+        # Ensure that non-existent lawns throw a 404
+        resp = self.client.get('planner/lawn/3333/')
+        self.assertEqual(resp.status_code, 404)
