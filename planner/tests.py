a8f701664727114bc917d32158be5ff70597cd69
Patrick W. Montgomery <pmontgo33@gmail.com>
2017-11-21 23:28:57 -0500

    Created fake unittest and changed postgres user permissions
    User can now create databases. This is necessary for testing.

--- planner/tests.py
+++ planner/tests.py
@@ -1,34 +1,38 @@
 """
-This file contains all of the tests for the planner django app
+This file contains all of the unit tests for the planner django app
 """
 
 # Import Statements
 from django.test import TestCase
-from django.contrib.auth.models import User
-from planner.models import Lawn, GrassType
 
 
-class PlannerViewsTestCase(TestCase):
-    fixtures = ['planner_views_testdata.json', 'auth_views_testdata.json']
+class SmokeTest(TestCase):
 
-    def test_index(self):
-        """
-        test if homepage (index) loads.
-        :return: 
-        """
-        resp = self.client.get('/')
-        self.assertEqual(resp.status_code, 200)
+    def test_bad_maths(self):
+        self.assertEqual(1 + 1, 3)
 
-    def test_detail(self):
-        """
-        test if lawn detail page loads.
-        :return: 
-        """
-        resp = self.client.get('/planner/lawn/1/')
-        self.assertEqual(resp.status_code, 200)
-        self.assertEqual(resp.context['lawn'].pk, 1)
-        self.assertEqual(resp.context['lawn'].name, "ORELAND PA Lawn")
 
-        # Ensure that non-existent lawns throw a 404
-        resp = self.client.get('planner/lawn/3333/')
-        self.assertEqual(resp.status_code, 404)
+# class PlannerViewsTestCase(TestCase):
+#     fixtures = ['planner_views_testdata.json', 'auth_views_testdata.json']
+#
+#     def test_index(self):
+#         """
+#         test if homepage (index) loads.
+#         :return:
+#         """
+#         resp = self.client.get('/')
+#         self.assertEqual(resp.status_code, 200)
+#
+#     def test_detail(self):
+#         """
+#         test if lawn detail page loads.
+#         :return:
+#         """
+#         resp = self.client.get('/planner/lawn/1/')
+#         self.assertEqual(resp.status_code, 200)
+#         self.assertEqual(resp.context['lawn'].pk, 1)
+#         self.assertEqual(resp.context['lawn'].name, "ORELAND PA Lawn")
+#
+#         # Ensure that non-existent lawns throw a 404
+#         resp = self.client.get('planner/lawn/3333/')
+#         self.assertEqual(resp.status_code, 404)
