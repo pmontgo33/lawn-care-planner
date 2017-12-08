f5327c4c1212884277c132fe1897d323b90c20a6
Patrick W. Montgomery <pmontgo33@gmail.com>
2017-12-07 22:59:56 -0500

    Added Invalid Zip Functional Test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -12,7 +12,8 @@ MAX_WAIT = 10
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
-    # fixtures = ['planner_grasstype_testdata.json']
+    fixtures = ['auth_views_testdata.json', 'planner_grasstype_testdata.json', 'planner_lawn_testdata.json',
+                'planner_lawnproduct_testdata.json', 'planner_weatherstation_testdata.json']
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -84,15 +85,40 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_cannot_submit_lawn_with_invalid_zip_code(self):
         # User visits LCP homepage
+        self.browser.get(self.live_server_url)
 
         # User clicks on planner link in banner
+        self.browser.find_element_by_link_text("Planner").click()
+        self.wait_for_element('id_zip_code')
 
         # Users enters an invalid zip code, but a selects a valid grass type, and enters a valid size
+        uls = self.browser.find_elements_by_tag_name('ul')
+        self.assertNotIn('errorlist', [ul.get_attribute('class') for ul in uls])
+        zip_input = self.browser.find_element_by_id('id_zip_code')
+        zip_input.send_keys('55555')
+
+        type_input = Select(self.browser.find_element_by_id('id_grass_type'))
+        type_input.select_by_visible_text('Perennial Ryegrass')
+
+        size_input = self.browser.find_element_by_id('id_size')
+        size_input.send_keys('5000')
 
         # When user presses clicks submit, he sees an error message
+        self.browser.find_element_by_class_name("save").click()
+        time.sleep(1)
+        uls = self.browser.find_elements_by_tag_name('ul')
+        self.assertIn('errorlist', [ul.get_attribute('class') for ul in uls])
 
         # User changes zip code to a valid zip code, and clicks submit again
+        zip_input = self.browser.find_element_by_id('id_zip_code')
+        zip_input.clear()
+        zip_input.send_keys('60652')
+        self.browser.find_element_by_class_name("save").click()
+        self.wait_for_element('id_lawn_name')
 
         # User is taken to a page that displays his lawn planner for the year
+        lawn_name = self.browser.find_element_by_id('id_lawn_name').text
+        self.assertIn('Lawn Name: ', lawn_name)
 
-        self.fail('write me!')
+        weather_station = self.browser.find_element_by_id('id_weather_station').text
+        self.assertIn('Closest Weather Station: ', weather_station)
--- mysite/settings.py
+++ mysite/settings.py
@@ -224,7 +224,7 @@ LOGGING = {
             'class': 'django.utils.log.AdminEmailHandler'
         },
         'console': {
-            'level': 'DEBUG',
+            'level': 'ERROR',
             'class': 'logging.StreamHandler',
             'formatter': 'main_formatter',
         },
